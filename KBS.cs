4f0b74f93807509593bd57cab0bc1be44553415b
qjatlr4009-max <qjatlr4009@gmail.com>
2026-01-20 13:11:05 +0900

    1팀 최고

---
+++ KBS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshCheck_CV
+{
+    internal class KBS
+    {
+        // 1팀 최고
+    }
+}
