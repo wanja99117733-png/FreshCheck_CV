3656797d9d723876aee7fef9085dc0a0ee09e6f9
Sleepy <leem089333@gmail.com>
2026-01-21 10:51:37 +0900

    로그폼 누락 고침

--- LogForm.Designer.cs
+++ LogForm.Designer.cs
@@ -29,9 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBoxLogs = new System.Windows.Forms.ListBox();
+
+            this.SuspendLayout();
+
+            // 
+            // listBoxLogs
+            // 
+            this.listBoxLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxLogs.FormattingEnabled = true;
+            this.listBoxLogs.HorizontalScrollbar = true;
+            this.listBoxLogs.IntegralHeight = false;
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.TabIndex = 0;
+
+            // 
+            // LogForm
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxLogs);
+            this.Name = "LogForm";
             this.Text = "LogForm";
+
+            this.ResumeLayout(false);
         }
 
         #endregion
