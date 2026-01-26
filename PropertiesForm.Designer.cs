56399c023254292a07618a961271e15286b0794c
ypark0615 <ypark0615hp@gmail.com>
2026-01-27 05:20:26 +0900

    Mold에 검은 배경 탭 컨트롤 적용

--- PropertiesForm.Designer.cs
+++ PropertiesForm.Designer.cs
@@ -28,29 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabPropControl = new System.Windows.Forms.TabControl();
+            this.tabPropControl = new FreshCheck_CV.UIControl.DarkTabControl();
             this.SuspendLayout();
             // 
             // tabPropControl
             // 
             this.tabPropControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPropControl.ItemSize = new System.Drawing.Size(100, 30);
             this.tabPropControl.Location = new System.Drawing.Point(0, 0);
             this.tabPropControl.Name = "tabPropControl";
             this.tabPropControl.SelectedIndex = 0;
-            this.tabPropControl.Size = new System.Drawing.Size(1278, 723);
-            this.tabPropControl.TabIndex = 0;
-            this.tabPropControl.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
-            this.tabPropControl.ItemSize = new System.Drawing.Size(0, 1);
+            this.tabPropControl.Size = new System.Drawing.Size(895, 482);
             this.tabPropControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
-            this.tabPropControl.BackColor = System.Drawing.Color.FromArgb(32, 36, 40);
-
+            this.tabPropControl.TabIndex = 0;
             // 
             // PropertiesForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1278, 723);
+            this.ClientSize = new System.Drawing.Size(895, 482);
             this.Controls.Add(this.tabPropControl);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PropertiesForm";
             this.Text = "PropertiesForm";
             this.ResumeLayout(false);
@@ -59,6 +57,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabPropControl;
+        private UIControl.DarkTabControl tabPropControl;
     }
 }
