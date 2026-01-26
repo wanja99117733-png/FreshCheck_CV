6d81d6126620d3375b0c36d2d25e01cd07c015f1
ypark0615 <ypark0615hp@gmail.com>
2026-01-27 04:07:15 +0900

    검은 테마 탭컨트롤 작업중

--- ResultForm.Designer.cs
+++ ResultForm.Designer.cs
@@ -30,53 +30,62 @@
         {
             this.tabMain = new System.Windows.Forms.TabControl();
             this.tabResults = new System.Windows.Forms.TabPage();
-            this.tabLogs = new System.Windows.Forms.TabPage();
-            this.panelTop = new System.Windows.Forms.Panel();
             this.dgvResults = new System.Windows.Forms.DataGridView();
-            this.listBoxLogs = new System.Windows.Forms.ListBox();
-            this.cbResultFilter = new System.Windows.Forms.ComboBox();
-            this.btnClear = new System.Windows.Forms.Button();
+            this.panelTop = new System.Windows.Forms.Panel();
             this.btnExportCsv = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cbResultFilter = new System.Windows.Forms.ComboBox();
+            this.tabLogs = new System.Windows.Forms.TabPage();
+            this.listBoxLogs = new System.Windows.Forms.ListBox();
+            this.darkTabControl1 = new FreshCheck_CV.UIControl.DarkTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabMain.SuspendLayout();
             this.tabResults.SuspendLayout();
-            this.tabLogs.SuspendLayout();
-            this.panelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.panelTop.SuspendLayout();
+            this.tabLogs.SuspendLayout();
+            this.darkTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabMain
             // 
             this.tabMain.Controls.Add(this.tabResults);
             this.tabMain.Controls.Add(this.tabLogs);
-            this.tabMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabMain.Location = new System.Drawing.Point(0, 0);
+            this.tabMain.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tabMain.Location = new System.Drawing.Point(341, 0);
+            this.tabMain.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabMain.Name = "tabMain";
             this.tabMain.SelectedIndex = 0;
-            this.tabMain.Size = new System.Drawing.Size(781, 540);
+            this.tabMain.Size = new System.Drawing.Size(552, 675);
             this.tabMain.TabIndex = 1;
             // 
             // tabResults
             // 
             this.tabResults.Controls.Add(this.dgvResults);
             this.tabResults.Controls.Add(this.panelTop);
-            this.tabResults.Location = new System.Drawing.Point(4, 22);
+            this.tabResults.Location = new System.Drawing.Point(4, 25);
+            this.tabResults.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabResults.Name = "tabResults";
-            this.tabResults.Padding = new System.Windows.Forms.Padding(3);
-            this.tabResults.Size = new System.Drawing.Size(773, 514);
+            this.tabResults.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabResults.Size = new System.Drawing.Size(544, 646);
             this.tabResults.TabIndex = 0;
             this.tabResults.Text = "Results";
             this.tabResults.UseVisualStyleBackColor = true;
             // 
-            // tabLogs
+            // dgvResults
             // 
-            this.tabLogs.Controls.Add(this.listBoxLogs);
-            this.tabLogs.Location = new System.Drawing.Point(4, 22);
-            this.tabLogs.Name = "tabLogs";
-            this.tabLogs.Padding = new System.Windows.Forms.Padding(3);
-            this.tabLogs.Size = new System.Drawing.Size(773, 514);
-            this.tabLogs.TabIndex = 1;
-            this.tabLogs.Text = "Logs";
-            this.tabLogs.UseVisualStyleBackColor = true;
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvResults.Location = new System.Drawing.Point(3, 33);
+            this.dgvResults.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.ReadOnly = true;
+            this.dgvResults.RowHeadersWidth = 51;
+            this.dgvResults.RowTemplate.Height = 23;
+            this.dgvResults.Size = new System.Drawing.Size(538, 609);
+            this.dgvResults.TabIndex = 1;
             // 
             // panelTop
             // 
@@ -84,32 +93,31 @@
             this.panelTop.Controls.Add(this.btnClear);
             this.panelTop.Controls.Add(this.cbResultFilter);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(3, 3);
+            this.panelTop.Location = new System.Drawing.Point(3, 4);
+            this.panelTop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(767, 23);
+            this.panelTop.Size = new System.Drawing.Size(538, 29);
             this.panelTop.TabIndex = 0;
             // 
-            // dgvResults
+            // btnExportCsv
             // 
-            this.dgvResults.AllowUserToAddRows = false;
-            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvResults.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvResults.Location = new System.Drawing.Point(3, 26);
-            this.dgvResults.Name = "dgvResults";
-            this.dgvResults.ReadOnly = true;
-            this.dgvResults.RowTemplate.Height = 23;
-            this.dgvResults.Size = new System.Drawing.Size(767, 485);
-            this.dgvResults.TabIndex = 1;
+            this.btnExportCsv.Location = new System.Drawing.Point(238, -1);
+            this.btnExportCsv.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExportCsv.Name = "btnExportCsv";
+            this.btnExportCsv.Size = new System.Drawing.Size(125, 29);
+            this.btnExportCsv.TabIndex = 2;
+            this.btnExportCsv.Text = "Export CSV";
+            this.btnExportCsv.UseVisualStyleBackColor = true;
             // 
-            // listBoxLogs
+            // btnClear
             // 
-            this.listBoxLogs.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxLogs.FormattingEnabled = true;
-            this.listBoxLogs.ItemHeight = 12;
-            this.listBoxLogs.Location = new System.Drawing.Point(3, 3);
-            this.listBoxLogs.Name = "listBoxLogs";
-            this.listBoxLogs.Size = new System.Drawing.Size(767, 508);
-            this.listBoxLogs.TabIndex = 0;
+            this.btnClear.Location = new System.Drawing.Point(145, -1);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(86, 29);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
             // 
             // cbResultFilter
             // 
@@ -119,41 +127,91 @@
             "OK",
             "NG"});
             this.cbResultFilter.Location = new System.Drawing.Point(2, 0);
+            this.cbResultFilter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cbResultFilter.Name = "cbResultFilter";
-            this.cbResultFilter.Size = new System.Drawing.Size(121, 20);
+            this.cbResultFilter.Size = new System.Drawing.Size(138, 23);
             this.cbResultFilter.TabIndex = 0;
             // 
-            // btnClear
+            // tabLogs
             // 
-            this.btnClear.Location = new System.Drawing.Point(127, -1);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 23);
-            this.btnClear.TabIndex = 1;
-            this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.tabLogs.Controls.Add(this.listBoxLogs);
+            this.tabLogs.Location = new System.Drawing.Point(4, 25);
+            this.tabLogs.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabLogs.Name = "tabLogs";
+            this.tabLogs.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabLogs.Size = new System.Drawing.Size(395, 808);
+            this.tabLogs.TabIndex = 1;
+            this.tabLogs.Text = "Logs";
+            this.tabLogs.UseVisualStyleBackColor = true;
             // 
-            // btnExportCsv
+            // listBoxLogs
             // 
-            this.btnExportCsv.Location = new System.Drawing.Point(208, -1);
-            this.btnExportCsv.Name = "btnExportCsv";
-            this.btnExportCsv.Size = new System.Drawing.Size(109, 23);
-            this.btnExportCsv.TabIndex = 2;
-            this.btnExportCsv.Text = "Export CSV";
-            this.btnExportCsv.UseVisualStyleBackColor = true;
+            this.listBoxLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxLogs.FormattingEnabled = true;
+            this.listBoxLogs.ItemHeight = 15;
+            this.listBoxLogs.Location = new System.Drawing.Point(3, 4);
+            this.listBoxLogs.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.Size = new System.Drawing.Size(608, 1000);
+            this.listBoxLogs.TabIndex = 0;
+            // 
+            // darkTabControl1
+            // 
+            this.darkTabControl1.Controls.Add(this.tabPage1);
+            this.darkTabControl1.Controls.Add(this.tabPage2);
+            this.darkTabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.darkTabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.darkTabControl1.HeaderBackColor = System.Drawing.Color.Black;
+            this.darkTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.darkTabControl1.Name = "darkTabControl1";
+            this.darkTabControl1.SelectedIndex = 0;
+            this.darkTabControl1.Size = new System.Drawing.Size(321, 675);
+            this.darkTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.darkTabControl1.TabBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(34)))), ((int)(((byte)(40)))));
+            this.darkTabControl1.TabBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.darkTabControl1.TabForeColor = System.Drawing.Color.Gainsboro;
+            this.darkTabControl1.TabIndex = 2;
+            this.darkTabControl1.TabSelectedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.darkTabControl1.TabSelectedForeColor = System.Drawing.Color.White;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(34)))), ((int)(((byte)(40)))));
+            this.tabPage1.ForeColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(250, 513);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(34)))), ((int)(((byte)(40)))));
+            this.tabPage2.ForeColor = System.Drawing.Color.LightGray;
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(391, 502);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
             // 
             // ResultForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 540);
+            this.ClientSize = new System.Drawing.Size(893, 675);
+            this.Controls.Add(this.darkTabControl1);
             this.Controls.Add(this.tabMain);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ResultForm";
             this.Text = "LogForm";
             this.tabMain.ResumeLayout(false);
             this.tabResults.ResumeLayout(false);
-            this.tabLogs.ResumeLayout(false);
-            this.panelTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.panelTop.ResumeLayout(false);
+            this.tabLogs.ResumeLayout(false);
+            this.darkTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -168,5 +226,8 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.ComboBox cbResultFilter;
         private System.Windows.Forms.ListBox listBoxLogs;
+        private UIControl.DarkTabControl darkTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
---
+++ UIControl/DarkTabControl.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FreshCheck_CV.UIControl
+{
+    public class DarkTabControl : TabControl
+    {
+        [Category("DarkTheme")]
+        public Color HeaderBackColor { get; set; } = Color.FromArgb(30, 34, 40);
+
+        [Category("DarkTheme")]
+        public Color BorderColor { get; set; } = Color.Black;
+
+        [Category("DarkTheme")]
+        public Color TabBackColor { get; set; } = Color.FromArgb(30, 34, 40);
+
+        [Category("DarkTheme")]
+        public Color TabSelectedBackColor { get; set; } = Color.FromArgb(45, 45, 48);
+
+        [Category("DarkTheme")]
+        public Color TabForeColor { get; set; } = Color.LightGray;
+
+        [Category("DarkTheme")]
+        public Color TabSelectedForeColor { get; set; } = Color.White;
+
+        [Category("DarkTheme")]
+        public Color TabBorderColor { get; set; } = Color.FromArgb(70, 70, 70);
+
+        public DarkTabControl()
+        {
+            //SetStyle(ControlStyles.UserPaint |
+            //         ControlStyles.AllPaintingInWmPaint |
+            //         ControlStyles.OptimizedDoubleBuffer |
+            //         ControlStyles.ResizeRedraw, true);
+
+            SetStyle(ControlStyles.AllPaintingInWmPaint |
+                     ControlStyles.OptimizedDoubleBuffer |
+                     ControlStyles.ResizeRedraw, true);
+
+            DrawMode = TabDrawMode.OwnerDrawFixed;
+            SizeMode = TabSizeMode.Fixed;
+
+
+        }
+
+        protected override void OnControlAdded(ControlEventArgs e)
+        {
+            base.OnControlAdded(e);
+
+            if (e.Control is TabPage page)
+            {
+                page.BackColor = TabBackColor;
+                page.ForeColor = TabForeColor;
+            }
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            var page = TabPages[e.Index];
+            var r = GetTabRect(e.Index);
+
+            bool selected = (e.Index == SelectedIndex);
+
+            Color back = selected ? TabSelectedBackColor : TabBackColor;
+            Color fore = selected ? TabSelectedForeColor : TabForeColor;
+
+            using (var b = new SolidBrush(back))
+                e.Graphics.FillRectangle(b, r);
+
+            using (var p = new Pen(TabBorderColor))
+                e.Graphics.DrawRectangle(p, r);
+
+            TextRenderer.DrawText(
+                e.Graphics,
+                page.Text,
+                Font,
+                r,
+                fore,
+                TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+        //protected override void OnPaintBackground(PaintEventArgs e)
+        //{
+        //    // 기본 배경 처리 먼저(페이지/컨트롤 영역)
+        //    base.OnPaintBackground(e);
+
+        //    // 그 다음 헤더 스트립만 칠하기 (탭은 이후 OnDrawItem에서 그려짐)
+        //    int headerHeight = DisplayRectangle.Y;
+        //    if (headerHeight > 0)
+        //    {
+        //        var headerRect = new Rectangle(0, 0, Width, headerHeight);
+        //        using (var b = new SolidBrush(HeaderBackColor))
+        //            e.Graphics.FillRectangle(b, headerRect);
+        //    }
+        //}
+
+        //protected override void OnPaint(PaintEventArgs e)
+        //{
+        //    // 여기서 헤더를 다시 칠하면 탭이 가려짐 → base만 호출
+        //    base.OnPaint(e);
+        //}
+
+        private const int WM_PAINT = 0x000F;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_PAINT)
+                PaintHeaderStripExcludeTabs();
+        }
+
+        private void PaintHeaderStripExcludeTabs()
+        {
+            int headerHeight = DisplayRectangle.Y;
+            if (headerHeight <= 0) return;
+
+            var headerRect = new Rectangle(0, 0, Width, headerHeight);
+
+            using (var g = Graphics.FromHwnd(Handle))
+            using (var brush = new SolidBrush(HeaderBackColor))
+            using (var region = new Region(headerRect))
+            {
+                // 탭 버튼 영역은 절대 덮지 않음
+                for (int i = 0; i < TabCount; i++)
+                    region.Exclude(GetTabRect(i));
+
+                g.FillRegion(brush, region);
+            }
+        }
+
+    }
+}
